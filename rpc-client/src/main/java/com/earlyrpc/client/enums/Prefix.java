93487243a1448a05503fb2e91cdd4fd8396e3aa6
君寻 <7493658+solthx@user.noreply.gitee.com>
2020-08-18 18:36:38 +0800

    实现了rpc接口的bean在容器中的beanName前缀

---
+++ rpc-client/src/main/java/com/earlyrpc/client/enums/Prefix.java
@@ -0,0 +1,9 @@
+package com.earlyrpc.client.enums;
+
+/**
+ * @author: czf
+ * @date: 2020/8/18 13:56
+ */
+public class Prefix {
+    public static String ERPC_CONSUMER_BEANNAME_PREFIX = "erpc_";
+}
