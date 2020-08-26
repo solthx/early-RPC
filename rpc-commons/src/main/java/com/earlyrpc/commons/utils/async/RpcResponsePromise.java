c01176588d9339e3bbbbbeac4f47a1b83ecb4a38
君寻 <7493658+solthx@user.noreply.gitee.com>
2020-08-26 08:45:32 +0800

    2020/08/26 8:45 a.m

--- rpc-commons/src/main/java/com/earlyrpc/commons/utils/async/RpcResponsePromise.java
+++ rpc-commons/src/main/java/com/earlyrpc/commons/utils/async/RpcResponsePromise.java
@@ -1,9 +1,7 @@
 package com.earlyrpc.commons.utils.async;
 
 import com.earlyrpc.commons.protocol.RpcResponse;
-import io.netty.channel.ChannelPromise;
 import io.netty.util.concurrent.DefaultPromise;
-import io.netty.util.concurrent.EventExecutor;
 
 /**
  * @author czf
