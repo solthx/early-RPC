3f306648a0548d3290e2f2fb4364f24ff7024cee
君寻 <7493658+solthx@user.noreply.gitee.com>
2020-08-26 08:42:18 +0800

    ..

---
+++ rpc-commons/src/main/java/com/earlyrpc/commons/utils/async/RpcResponsePromise.java
@@ -0,0 +1,11 @@
+package com.earlyrpc.commons.utils.async;
+
+import com.earlyrpc.commons.protocol.RpcResponse;
+import io.netty.util.concurrent.DefaultPromise;
+
+/**
+ * @author: czf
+ * @date: 2020/8/19 15:07
+ */
+public class RpcResponsePromise extends DefaultPromise<RpcResponse> {
+}
